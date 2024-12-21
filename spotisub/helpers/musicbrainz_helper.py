d09c221795a16a77babd8306f7e91c7f737feb07
gabe <gabeklav@fastmail.com>
2024-12-21 01:09:20 -0500

    nicer logging of MusicBrainz api errors

--- spotisub/helpers/musicbrainz_helper.py
+++ spotisub/helpers/musicbrainz_helper.py
@@ -4,6 +4,7 @@ import logging
 
 
 import musicbrainzngs
+from musicbrainzngs.musicbrainz import ResponseError
 from spotisub import spotisub
 from spotisub import utils
 
@@ -18,11 +19,19 @@ musicbrainzngs.set_useragent(
     "0.1",
     "http://example.com/music")
 
-def get_mbids_from_isrc(isrc) -> list:
+def get_mbids_from_isrc(isrc: str) -> list:
     try:
-        res = musicbrainzngs.get_recordings_by_isrc(isrc, includes=["isrcs"])
+        res = musicbrainzngs.get_recordings_by_isrc(isrc)
         time.sleep(0.25)
-    except BaseException:
+    except ResponseError as e:
+        if "404" in str(e):
+            logging.error(f'Spotify track with ISRC: {isrc} was not found in the MusicBrainz database. Consider manually submitting it.')
+        elif "400" in str(e):
+            logging.error(f'HTTP Error 400 from MusicBrainz API for ISRC: {isrc}.')
+        else:
+            utils.write_exception()
+        return []
+    except Exception:
         utils.write_exception()
         return []
 
