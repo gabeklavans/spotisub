4d0a6c41c0d4b068d7ceb992553bc8e80862e5d7
gabe <gabeklav@fastmail.com>
2024-12-24 20:04:31 -0500

    guard artist top track playlists with env var, default to OFF

--- spotisub/generator.py
+++ spotisub/generator.py
@@ -633,7 +633,8 @@ def reimport(uuid):
             #     constants.ARTIST_GEN_SCHED,
             #     constants.ARTIST_GEN_SCHED_DEFAULT_VALUE)
             pass
-        elif playlist_info.type == constants.JOB_ATT_ID:
+        elif (playlist_info.type == constants.JOB_ATT_ID and 
+              os.environ.get(constants.ARTIST_PLAYLIST_ENABLED, constants.ARTIST_PLAYLIST_ENABLED) == "1"):
             run_job_now(
                 timedelta_sec,
                 artist_top_tracks,
@@ -754,7 +755,8 @@ def init_jobs():
     # https://developer.spotify.com/blog/2024-11-27-changes-to-the-web-api
     # init_my_recommendations()
     # init_artists_recommendations()
-    init_artists_top_tracks()
+    if os.environ.get(constants.ARTIST_PLAYLIST_ENABLED, constants.ARTIST_PLAYLIST_ENABLED) == "1":
+        init_artists_top_tracks()
     init_user_playlists()
 
 
@@ -765,7 +767,8 @@ def scan_library():
     # https://developer.spotify.com/blog/2024-11-27-changes-to-the-web-api
     # scan_my_recommendations()
     # scan_artists_recommendations()
-    scan_artists_top_tracks()
+    if os.environ.get(constants.ARTIST_PLAYLIST_ENABLED, constants.ARTIST_PLAYLIST_ENABLED) == "1":
+        scan_artists_top_tracks()
     scan_user_playlists()
 
 
@@ -786,8 +789,9 @@ def reimport_all_thread():
     # https://developer.spotify.com/blog/2024-11-27-changes-to-the-web-api
     # import_all_my_recommendations()
     # import_all_artists_recommendations()
+    if os.environ.get(constants.ARTIST_PLAYLIST_ENABLED, constants.ARTIST_PLAYLIST_ENABLED) == "1":
+        import_all_artists_top_tracks()
     import_all_user_playlists()
-    import_all_artists_top_tracks()
 
 
 def import_all_user_saved_tracks():
